e4427d4c5316dfb84e4d9e08fbd80aa53ca55b08
aryaddinata <aryaddinataabbas017@gmail.com>
2024-05-16 22:30:10 +0900

    feat, chore, refactor : dashboard #1

---
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainLayout from "../../../layouts/MainLayout";
+
+const Dashboard = () => {
+  return (
+    <>
+      <MainLayout>
+        <h1>Dashboard Pages</h1>
+      </MainLayout>
+    </>
+  );
+};
+
+export default Dashboard;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import PrivateRoute from "./PrivateRoute";
 import Beranda from "../pages/beranda/Beranda";
 import Berita from "../pages/berita/Berita";
 import DetailBerita from "../pages/berita/DetailBerita";
@@ -8,61 +7,29 @@ import EmailVerification from "../pages/auth/EmailVerification";
 import OtpVerification from "../pages/auth/OtpVerification";
 import ResetPassword from "../pages/auth/ResetPassword";
 
+import Dashboard from "../pages/admin/dashboard/Dashboard";
+import Tabel from "../pages/admin/tabel/Tabel";
 
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Beranda />,
-//   },
-//   {
-//     path: "/berita",
-//     element: <Berita />,
-//   },
-//   {
-//     path: "/berita/:slug",
-//     element: <DetailBerita />,
-//   },
-// ]);
-
-const AppRoutes = () => {
-  // logic fetchTypeUser (admin & user)
-
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(fetchUserType());
-  // }, [dispatch]);
+const AppRoutes = () =>
+{
 
   return (
     <Router>
       <Routes>
-        {/*** General Route ***/}
+        {/*** General Route ***/ }
 
-        <Route path="/sign-in" element={<SignIN />} />
-        <Route path="/email-verification" element={<EmailVerification />} />
-        <Route path="/otp-verification" element={<OtpVerification />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/" element={<Beranda />} />
-        <Route path="/berita" element={<Berita />} />
-        <Route path="/berita/:slug" element={<DetailBerita />} />
+        <Route path="/sign-in" element={ <SignIN /> } />
+        <Route path="/email-verification" element={ <EmailVerification /> } />
+        <Route path="/otp-verification" element={ <OtpVerification /> } />
+        <Route path="/reset-password" element={ <ResetPassword /> } />
+        <Route path="/" element={ <Beranda /> } />
+        <Route path="/berita" element={ <Berita /> } />
+        <Route path="/berita/:slug" element={ <DetailBerita /> } />
 
-        {/*** Private Route ***/}
+        {/* Dashboard Route */ }
+        <Route path="/dashboard" element={ <Dashboard /> } />
+        <Route path="/tabel" element={ <Tabel /> } />
 
-        {/* <Route
-          path="/"
-          element={
-            <PrivateRoute
-              element={<Dashboard />}
-              roles={["admin", "user"]}
-            />
-          }
-        />
-        <Route
-          path="/data/projek"
-          element={
-            <PrivateRoute element={<Projek />} roles={["admin", "user"]} />
-          }
-        /> */}
       </Routes>
     </Router>
   );
