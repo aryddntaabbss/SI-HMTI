3c88fe3d2e92f731daded6b203a7c789c1e0908d
Asil Taufiqurrahman <asilt94@gmail.com>
2024-06-12 16:49:56 +0900

    refactor: remove unused props

--- src/components/BidangPengurus.jsx
+++ src/components/BidangPengurus.jsx
@@ -1,4 +1,4 @@
-const BidangPengurus = ({ id, title, children }) => {
+const BidangPengurus = ({ children }) => {
   return (
     <>
       <div className="flex justify-center gap-14 py-10">{children}</div>
