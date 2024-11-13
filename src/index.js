9597c5d1654fafab45b36aab9fb785de3fa12753
aryaddinata <aryaddinataabbas017@gmail.com>
2024-11-14 01:50:15 +0900

    HMTI V.2.0.0

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import store from "./redux/store";
 import App from "./App";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Mengimpor fungsi untuk mendaftarkan service worker
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+// Membuat root untuk aplikasi React
+const root = ReactDOM.createRoot( document.getElementById( "root" ) );
 root.render(
-  <>
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  </>
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
+// Mendaftarkan service worker untuk mengaktifkan PWA
+serviceWorkerRegistration.register();
